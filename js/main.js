bcf9bab8bafecf6a9558759e196b7ffc946f5e31
Talles <teduardo13@hotmail.com>
2019-10-15 20:36:18 -0300

    Adicionado Menu de Login

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$( "#modalPushLogin" ).click(function() {
+    var opacity = $('#siteinicial').css({opacity: "1", backgroundColor: "" });
+});
+
+$("#cardpushlogin").click( function(){
+    var opacity = $('#siteinicial').css({opacity: "0.3", backgroundColor: "rgba(0,0,0,0.8)" });
+});
