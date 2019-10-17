fbcfc8eeaa1afdd90a922fbf1cfcb6270eba9404
Talles <teduardo13@hotmail.com>
2019-10-16 21:33:15 -0300

    Alterando card Consulta

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ $( "#modalPushLogin" ).click(function() {
 $("#cardpushlogin").click( function(){
     var opacity = $('#siteinicial').css({opacity: "0.3", backgroundColor: "rgba(0,0,0,0.8)" });
 });
+
+
+$( "#modalPushConsulta" ).click(function() {
+    var opacity = $('#siteinicial').css({opacity: "1", backgroundColor: "" });
+});
+
+$("#cardpushconsulta").click( function(){
+    var opacity = $('#siteinicial').css({opacity: "0.3", backgroundColor: "rgba(0,0,0,0.8)" });
+});
